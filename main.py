30f071c4312f4df1cbce0f36f4d6f13ad03c876b
Siddiqui Abdul Moid - 58 <siddiquimoid@eng.rizvi.edu.in>
2026-02-16 03:00:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-# AIM: Develop a Python program that reads a text file and 
-# prints words of specified lengths (e.g., three, four, 
-# five, etc.) found within the file.
-# Coder:
-# Date:
+length = int(input("Enter Length of Words: "))
 
-print("--- Extracting Words from Text File ---\n")
+with open("story.txt", "r") as file:
+    text = file.read()
 
+words = text.split()
+
+result = set()
+
+for word in words:
+    word_lower = word.lower()
+    if len(word_lower) == length:
+        result.add(word_lower)
+
+sorted_words = sorted(result)
+
+print(f"Following Unique words of length {length} present: {sorted_words}")
